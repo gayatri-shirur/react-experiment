0864cb32c785d7c27d8f84a9e691c60e3c643f14
gayatri <gayatrimshirur@gmail.com>
2025-10-07 20:22:02 +0530

    API using useEffect

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import './App.css'
 
+
 function App() {
   const url = 'https://dummyjson.com/products';
+    
+  const [products, setProducts] = React.useState([])
+
+  React.useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products))
+      .catch((error) => console.error('Error fetching data:', error))
+  }, [])
 
   return (
     <div className="App">
-      <h1>React Experiment</h1>
+      <h1>Product Titles</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
 
-export default App
+export default App  
